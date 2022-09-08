b11fef66d455d3c999841a07fc3b7224df394e02
poppyLebenya <moiponelebenya@gmail.com>
2022-09-08 07:12:00 +0200

    Added packages for security

--- ConnectedOffice/Controllers/DevicesController.cs
+++ ConnectedOffice/Controllers/DevicesController.cs
@@ -72,7 +72,8 @@ namespace ConnectedOffice.Controllers
 
             return NoContent();
         }
-
+       
+        
         // POST: api/Devices
         // To protect from overposting attacks, enable the specific properties you want to bind to, for
         // more details, see https://go.microsoft.com/fwlink/?linkid=2123754.
